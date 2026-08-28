26687ad675e6aa72809793577816ef0272ae18b3
ozapcr <oza24si@mahasiswa.pcr.ac.id>
2026-08-29 03:30:15 +0700

    Fix HTTPS assets through tunnel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (request()->header('x-forwarded-proto') === 'https') {
+            URL::forceScheme('https');
+        }
+
         Gate::define('is-admin', fn ($user) => $user->isAdmin());
         Gate::define('is-receptionist-or-admin', fn ($user) => $user->isOperator());
     }
